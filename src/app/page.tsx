f7f89a0d18193a208fcf783e897c06910d608463
Thiago Rocha <thiago_srocha01@hotmail.com>
2025-10-13 22:41:28 -0300

    feat(0h25): inicia configuração do Drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-green-500">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
